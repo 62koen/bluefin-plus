dd7587873918f9f320b6331962ca20e1efe4e797
Koen <7tcxqcoo@addy.io>
2024-10-13 18:13:23 +0200

    Added VirtualBox with akmod; removed xpad folder

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -ouex pipefail
 
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E %fedora)"
 
 
@@ -18,9 +20,15 @@ rpm-ostree install ImageMagick
 rpm-ostree install ghostscript
 rpm-ostree install inotify-tools
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree install android-tools
+dnf install -y \
+    akmod-VirtualBox-*.fc${RELEASE}.${ARCH}
+akmods --force --kernels "${KERNEL}" --kmod VirtualBox
+modinfo /usr/lib/modules/${KERNEL}/extra/VirtualBox/{vboxdrv,vboxnetadp,vboxnetflt}.ko.xz > /dev/null \
+|| (find /var/cache/akmods/VirtualBox/ -name \*.log -print -exec cat {} \; && exit 1)
 
 # this would install a package from rpmfusion
-# rpm-ostree install VirtualBox
+rpm-ostree install VirtualBox
 
 #### Example for enabling a System Unit File
 
