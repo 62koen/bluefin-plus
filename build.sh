84a661806ea0205a39a28983e72c4ed3c6984c29
Koen <7tcxqcoo@addy.io>
2024-10-13 17:37:36 +0200

    Removed VB and xpad notes

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-# rpm-ostree install dkms
 rpm-ostree install jbigkit
 rpm-ostree install ImageMagick
 rpm-ostree install ghostscript
@@ -21,7 +20,7 @@ rpm-ostree install inotify-tools
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
 # this would install a package from rpmfusion
-rpm-ostree install VirtualBox
+# rpm-ostree install VirtualBox
 
 #### Example for enabling a System Unit File
 
