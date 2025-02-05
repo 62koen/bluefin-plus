a1c949afb1286bc6bae184261536c0b926accc87
62koen <ejeqyj57@addy.to>
2025-02-05 20:32:23 +0100

    Removed apps that can be installed in a container

--- build.sh
+++ build.sh
@@ -4,12 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-# repos
-rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | tee -a /etc/yum.repos.d/vscodium.repo
-rpm-ostree install \
-https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-
 # removes
 rpm-ostree override remove \
     gnome-classic-session \
@@ -25,11 +19,8 @@ jbigkit \
 ImageMagick \
 ghostscript \
 inotify-tools \
-android-tools \
-codium \
 mangohud \
-goverlay \
-java-latest-openjdk
+goverlay
 
 # extensions
 rpm-ostree install \
